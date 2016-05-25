2b1420842c6359c67f822b1dea4e6a603ed588d2
Matthias Meusburger <matthias.meusburger@biblibre.com>
2016-05-25 17:17:49 +0200

    RF41380: Add a step in a resource workflow
     - Do not display archived workflows steps
    
    NOTE: This patch is *NOT* on the dev branch and must
    be added to it if the "archiving a workflow" is merged
    before this one.

--- resources/ajax_forms/getCurrentWorkflowForm.php
+++ resources/ajax_forms/getCurrentWorkflowForm.php
@@ -8,8 +8,8 @@ if (!isset($_GET['resourceID'])){
     $userGroupObj = new UserGroup();
     $userGroupArray = $userGroupObj->allAsArray();
 
-    $resourceSteps = $resource->getResourceSteps();
-    $parentSteps = $resource->getResourceSteps();
+    $resourceSteps = $resource->getCurrentWorkflowResourceSteps();
+    $parentSteps = $resource->getCurrentWorkflowResourceSteps();
 
     // Get reminder, from the resourceSteps, or from the workflow
     foreach ($resourceSteps as $resourceStep) {
