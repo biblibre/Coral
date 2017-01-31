5c9c06cb706877ea7bbb04de613befc7b2a805bc
Matthias Meusburger <matthias.meusburger@biblibre.com>
2017-01-31 16:07:39 +0000

    GOKB rebase to master [WIP]

--- resources/install/protected/update_gokb.sql
+++ resources/install/protected/update_gokb.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `_DATABASE_NAME_`.`Identifier`; /*replace IsbnOrIssn table which can be deleted*/
-CREATE TABLE `_DATABASE_NAME_`.`Identifier` (
+DROP TABLE IF EXISTS `Identifier`; /*replace IsbnOrIssn table which can be deleted*/
+CREATE TABLE `Identifier` (
   `identifierID` int(11) NOT NULL auto_increment,
   `resourceID` int(11) NOT NULL,
   `identifierTypeID` int(11) default NULL,
@@ -14,22 +14,22 @@ INSERT INTO `Identifier` (`resourceID`, `identifier`)
 SELECT `resourceID`, `isbnOrIssn` FROM `IsbnOrIssn`;
 UPDATE `Identifier` SET `identifierTypeID`=1;
 
-DROP TABLE IF EXISTS `_DATABASE_NAME_`.`IsbnOrIssn`;
+DROP TABLE IF EXISTS `IsbnOrIssn`;
 
-DROP TABLE IF EXISTS `_DATABASE_NAME_`.`IdentifierType`;
-CREATE TABLE `_DATABASE_NAME_`.`IdentifierType` (
+DROP TABLE IF EXISTS `IdentifierType`;
+CREATE TABLE `IdentifierType` (
   `identifierTypeID` int(11) NOT NULL auto_increment,
   `identifierName` varchar(45) NOT NULL,
   PRIMARY KEY (`identifierTypeID`),
   UNIQUE KEY `identifierTypeID` (`identifierTypeID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO `_DATABASE_NAME_`.`IdentifierType` (identifierName) values('Isxn');
-INSERT INTO `_DATABASE_NAME_`.`IdentifierType` (identifierName) values('Issn');
-INSERT INTO `_DATABASE_NAME_`.`IdentifierType` (identifierName) values('Isbn');
-INSERT INTO `_DATABASE_NAME_`.`IdentifierType` (identifierName) values('eIssn');
-INSERT INTO `_DATABASE_NAME_`.`IdentifierType` (identifierName) values('eIsbn');
-INSERT INTO `_DATABASE_NAME_`.`IdentifierType` (identifierName) values('Gokb');
+INSERT INTO `IdentifierType` (identifierName) values('Isxn');
+INSERT INTO `IdentifierType` (identifierName) values('Issn');
+INSERT INTO `IdentifierType` (identifierName) values('Isbn');
+INSERT INTO `IdentifierType` (identifierName) values('eIssn');
+INSERT INTO `IdentifierType` (identifierName) values('eIsbn');
+INSERT INTO `IdentifierType` (identifierName) values('Gokb');
 
 
 
