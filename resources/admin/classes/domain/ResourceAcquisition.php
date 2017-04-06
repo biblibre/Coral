d247d9b48f69d26fb2f3b6b61af9d946923ec5b7
Matthias Meusburger <matthias.meusburger@biblibre.com>
2017-04-06 08:39:02 +0000

    Resource acquisition: multiple ordering
     - Create new order

--- resources/admin/classes/domain/ResourceAcquisition.php
+++ resources/admin/classes/domain/ResourceAcquisition.php
@@ -5,6 +5,34 @@ class ResourceAcquisition extends DatabaseObject {
 
 	protected function overridePrimaryKeyName() {}
 
+	//removes resource purchase sites
+	public function removePurchaseSites() {
+
+		$query = "DELETE
+			FROM ResourcePurchaseSiteLink
+			WHERE resourceAcquisitionID = '" . $this->resourceAcquisitionID . "'";
+
+		$result = $this->db->processQuery($query);
+	}
+
+	//returns array of purchase site objects
+	public function getPurchaseSites() {
+
+		$query = "SELECT PurchaseSite.* FROM PurchaseSite, ResourcePurchaseSiteLink RPSL where RPSL.purchaseSiteID = PurchaseSite.purchaseSiteID AND resourceAcquisitionID = '" . $this->resourceAcquisitionID . "'";
+
+		$result = $this->db->processQuery($query, 'assoc');
+
+		$objects = array();
+
+		//need to do this since it could be that there's only one request and this is how the dbservice returns result
+		if (isset($result['purchaseSiteID'])) { $result = [$result]; }
+		foreach ($result as $row) {
+			$object = new PurchaseSite(new NamedArguments(array('primaryKey' => $row['purchaseSiteID'])));
+			array_push($objects, $object);
+		}
+
+		return $objects;
+	}
 }
 
 ?>
