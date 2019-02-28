0c4b2683bbc247e72d1afab6ce5d86a3f7f6ad95
Coral User <support@biblibre.com>
2019-02-28 11:44:48 +0100

    no changers - just check

--- licensing/admin.php
+++ licensing/admin.php
@@ -32,8 +32,10 @@ if ($user->isAdmin()){
 
 <table class="headerTable">
 <tr><td>
+
 <span class="headerText"><?php echo _("Users");?></span>&nbsp;&nbsp;<span id='span_User_response' class='redText'></span>
-<br /><span id='span_newUser' class='adminAddInput'><a href='ajax_forms.php?action=getAdminUserUpdateForm&height=202&width=288&modal=true' class='thickbox' id='expression'><?php echo _("add new user");?></a></span>
+<span id='span_newUser' class='adminAddInput'><a href='ajax_forms.php?action=getAdminUserUpdateForm&height=202&width=288&modal=true' class='thickbox' id='expression'><?php echo _("add new user");?></a></span>
+
 <br /><br />
 <div id='div_User'>
 <img src = "images/circle.gif"><?php echo _("Loading...");?>
