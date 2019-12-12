f5bce565287fed3cfc7986ad1e8dc6af1bdfd8fc
BibLibre <support@biblibre.com>
2019-12-12 16:45:40 +0100

    Issue 458: Coral silently fails on too large file uploads
     - Fix merge

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -73,9 +73,6 @@ if (isset($customJSInclude)) {
             echo "<link rel='gettext' type='application/x-po' href='./locale/".$http_lang."/LC_MESSAGES/messages.po' />";
     }
 ?>
-<script type="text/javascript">
-Date.format = '<?php echo return_datepicker_date_format(); ?>';
-</script>
 </head>
 <body>
 <noscript><font face='arial'><?php echo _("JavaScript must be enabled in order for you to use CORAL. However, it seems JavaScript is either disabled or not supported by your browser. To use CORAL, enable JavaScript by changing your browser options, then ");?><a href=""><?php echo _("try again");?></a>. </font></noscript>
